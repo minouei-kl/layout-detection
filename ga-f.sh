28c16c38ff0e00da49a7f050e269bc6c08686aac
mohammad <mohammad.minooee@gmail.com>
2021-01-06 14:11:58 +0100

    crpn + ga

---
+++ ga-f.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip uninstall pycocotools -y
+pip install mmpycocotools
+pip install mmcv-full==1.2.5
+pip install git+file:///netscratch/minouei/report/mmdetection
+python -u tools/train.py configs/publay/ga_faster_x101_64x4d_freeze_fpn_1x_publay.py  --launcher=slurm
