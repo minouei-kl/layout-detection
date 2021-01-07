64d09f1d465f990585ca80e4624a7f44ba0c3332
mohammad <mohammad.minooee@gmail.com>
2021-01-07 19:16:39 +0100

    cascade dh

---
+++ dh-l.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip uninstall pycocotools -y
+pip install mmpycocotools
+pip install mmcv-full==1.2.5
+pip install git+file:///netscratch/minouei/report/mmdetection
+python -u tools/train.py configs/publay/dh_faster_rcnn_x101_fpn_1x_publay.py  --launcher=slurm
