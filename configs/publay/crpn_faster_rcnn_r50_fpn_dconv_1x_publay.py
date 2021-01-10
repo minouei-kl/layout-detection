2a26e68f20689fb859b86df5c6c9e685c951849e
minouei <mohammad.minooee@gmail.com>
2021-01-10 14:47:06 +0100

    cycle

--- configs/publay/crpn_faster_rcnn_r50_fpn_dconv_1x_publay.py
+++ configs/publay/crpn_faster_rcnn_r50_fpn_dconv_1x_publay.py
@@ -202,7 +202,7 @@ optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 lr_config = dict(
     policy='cyclic',
-    target_ratio=(10, 1),
+    target_ratio=(20, 1),
     cyclic_times=12,
     step_ratio_up=0.5,
 )
