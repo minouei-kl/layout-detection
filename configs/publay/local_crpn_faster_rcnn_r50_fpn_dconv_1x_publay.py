ab9882ed804c2b37f41a01951ec0c986968260e0
mohammad <mohammad.minooee@gmail.com>
2021-01-10 10:06:41 +0100

    local

---
+++ configs/publay/local_crpn_faster_rcnn_r50_fpn_dconv_1x_publay.py
@@ -0,0 +1,224 @@
+model = dict(
+    type='FasterRCNN',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=0,
+        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_eval=True,
+        style='pytorch',
+        dcn=dict(type='DCNv2', deform_groups=2, fallback_on_stride=False),
+        stage_with_dcn=(False, True, True, True)),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=5),
+    rpn_head=dict(
+        type='CascadeRPNHead',
+        num_stages=2,
+        stages=[
+            dict(
+                type='StageCascadeRPNHead',
+                in_channels=256,
+                feat_channels=256,
+                anchor_generator=dict(
+                    type='AnchorGenerator',
+                    scales=[8],
+                    ratios=[1.0],
+                    strides=[4, 8, 16, 32, 64]),
+                adapt_cfg=dict(type='dilation', dilation=3),
+                bridged_feature=True,
+                sampling=False,
+                with_cls=False,
+                reg_decoded_bbox=True,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=(0.0, 0.0, 0.0, 0.0),
+                    target_stds=(0.1, 0.1, 0.5, 0.5)),
+                loss_bbox=dict(type='IoULoss', linear=True, loss_weight=7.0)),
+            dict(
+                type='StageCascadeRPNHead',
+                in_channels=256,
+                feat_channels=256,
+                adapt_cfg=dict(type='offset'),
+                bridged_feature=False,
+                sampling=True,
+                with_cls=True,
+                reg_decoded_bbox=True,
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=(0.0, 0.0, 0.0, 0.0),
+                    target_stds=(0.05, 0.05, 0.1, 0.1)),
+                loss_cls=dict(
+                    type='CrossEntropyLoss', use_sigmoid=True,
+                    loss_weight=0.7),
+                loss_bbox=dict(type='IoULoss', linear=True, loss_weight=7.0))
+        ]),
+    roi_head=dict(
+        type='StandardRoIHead',
+        bbox_roi_extractor=dict(
+            type='SingleRoIExtractor',
+            roi_layer=dict(type='RoIAlign', output_size=7, sampling_ratio=0),
+            out_channels=256,
+            featmap_strides=[4, 8, 16, 32]),
+        bbox_head=dict(
+            type='Shared2FCBBoxHead',
+            in_channels=256,
+            fc_out_channels=1024,
+            roi_feat_size=7,
+            num_classes=5,
+            bbox_coder=dict(
+                type='DeltaXYWHBBoxCoder',
+                target_means=[0.0, 0.0, 0.0, 0.0],
+                target_stds=[0.04, 0.04, 0.08, 0.08]),
+            reg_class_agnostic=False,
+            loss_cls=dict(
+                type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.5),
+            loss_bbox=dict(type='SmoothL1Loss', loss_weight=1.0, beta=1.0))))
+train_cfg = dict(
+    rpn=[
+        dict(
+            assigner=dict(
+                type='RegionAssigner', center_ratio=0.2, ignore_ratio=0.5),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False),
+        dict(
+            assigner=dict(
+                type='MaxIoUAssigner',
+                pos_iou_thr=0.7,
+                neg_iou_thr=0.7,
+                min_pos_iou=0.3,
+                ignore_iof_thr=-1),
+            sampler=dict(
+                type='RandomSampler',
+                num=256,
+                pos_fraction=0.5,
+                neg_pos_ub=-1,
+                add_gt_as_proposals=False),
+            allowed_border=-1,
+            pos_weight=-1,
+            debug=False)
+    ],
+    rpn_proposal=dict(
+        nms_across_levels=False,
+        nms_pre=2000,
+        nms_post=1000,
+        max_num=300,
+        nms_thr=0.8,
+        min_bbox_size=0),
+    rcnn=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.65,
+            neg_iou_thr=0.65,
+            min_pos_iou=0.65,
+            match_low_quality=False,
+            ignore_iof_thr=-1),
+        sampler=dict(
+            type='RandomSampler',
+            num=256,
+            pos_fraction=0.25,
+            neg_pos_ub=-1,
+            add_gt_as_proposals=True),
+        pos_weight=-1,
+        debug=False))
+test_cfg = dict(
+    rpn=dict(
+        nms_across_levels=False,
+        nms_pre=1000,
+        nms_post=1000,
+        max_num=300,
+        nms_thr=0.8,
+        min_bbox_size=0),
+    rcnn=dict(
+        score_thr=0.001,
+        nms=dict(type='nms', iou_threshold=0.5),
+        max_per_img=100))
+dataset_type = 'CocoDataset'
+img_norm_cfg = dict(
+    mean=[30.3344, 30.4473, 30.3461], std=[56.7147, 56.9299, 56.8265], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale=(704, 704), keep_ratio=True),
+    dict(type='MyTransform'),
+    dict(type='RandomFlip', flip_ratio=0.0),
+    dict(
+        type='Normalize',
+        mean=[30.3344, 30.4473, 30.3461],
+        std=[56.7147, 56.9299, 56.8265],
+        to_rgb=True),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(704, 704),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='MyTransform'),
+            dict(type='RandomFlip'),
+            dict(
+                type='Normalize',
+                mean=[30.3344, 30.4473, 30.3461],
+                std=[56.7147, 56.9299, 56.8265],
+                to_rgb=True),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=4,
+    workers_per_gpu=8,
+    train=dict(
+        type='CocoDataset',
+        classes=('text', 'title', 'list', 'table', 'figure'),
+        ann_file='/home/mohammad/Documents/research/code/mmdetection/datasets/publaynet/train.json',
+        img_prefix='/home/mohammad/Documents/research/code/mmdetection/datasets/publaynet/train/',
+        pipeline=train_pipeline),
+    val=dict(
+        type='CocoDataset',
+        classes=('text', 'title', 'list', 'table', 'figure'),
+        ann_file='/home/mohammad/Documents/research/code/mmdetection/datasets/publaynet/val.json',
+        img_prefix='/home/mohammad/Documents/research/code/mmdetection/datasets/publaynet/val/',
+        pipeline=test_pipeline),
+    test=dict(
+        type='CocoDataset',
+        classes=('text', 'title', 'list', 'table', 'figure'),
+        ann_file='/home/mohammad/Documents/research/code/mmdetection/datasets/publaynet/val.json',
+        img_prefix='/home/mohammad/Documents/research/code/mmdetection/datasets/publaynet/val/',
+        pipeline=test_pipeline))
+evaluation = dict(interval=1, metric='bbox')
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11])
+total_epochs = 12
+checkpoint_config = dict(interval=1)
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='TensorboardLoggerHook')
+    ])
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+rpn_weight = 0.7
+work_dir = '/netscratch/minouei/report/work_dirs/crpn_faster_rcnn_r50_fpn_1x_coco'
+gpu_ids = range(0, 1)
