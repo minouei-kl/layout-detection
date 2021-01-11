abe4e8e29ae7ca48c6f05650f5eb0c99c13d9452
minouei <mohammad.minooee@gmail.com>
2021-01-11 14:02:02 +0100

    cycle

--- configs/publay/faster_rcnn_s50_fpn_syncbn-backbone+head_mstrain-range_cycle_publay.py
+++ configs/publay/faster_rcnn_s50_fpn_syncbn-backbone+head_mstrain-range_cycle_publay.py
@@ -218,9 +218,10 @@ optimizer = dict(type='SGD', lr=0.002, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 lr_config = dict(
     policy='cyclic',
-    target_ratio=(20, 1),
+    target_ratio=(20, 0.1),
     cyclic_times=12,
     step_ratio_up=0.5,
+    gamma=0.7
 )
 total_epochs = 12
 checkpoint_config = dict(interval=1)
@@ -236,5 +237,5 @@ load_from = None
 resume_from = None
 workflow = [('train', 1)]
 norm_cfg = dict(type='SyncBN', requires_grad=True)
-work_dir = '/netscratch/minouei/report/work_dirs/faster_rcnn_s50_fpn_syncbn-backbone+head_mstrain-range_cycle_publay'
+work_dir = '/netscratch/minouei/report/work_dirs/faster_rcnn_s50_fpn_syncbn-backbone+head_mstrain-range_cycle7_publay'
 gpu_ids = range(0, 1)
