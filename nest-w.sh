749be9839d970cd57c07f922cb7d336d33dfd37a
mohammad <mohammad.minooee@gmail.com>
2021-01-12 18:49:20 +0100

    nest

--- nest-w.sh
+++ nest-w.sh
@@ -3,4 +3,4 @@ pip uninstall pycocotools -y
 pip install mmpycocotools
 MMCV_WITH_OPS=1 pip install git+file:///netscratch/minouei/report/mmcv
 pip install git+file:///netscratch/minouei/report/mmdetection
-python -u tools/train.py configs/publay/faster_rcnn_s50_fpn_syncbn-backbone+head_mstrain-range_cycle_publay.py  --launcher=slurm
+python -u tools/train.py configs/publay/faster_rcnn_s50_fpn_wT_cycle_publay.py  --launcher=slurm
