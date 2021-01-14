7e1388b8a6401bb48757f7a98ad829cd7b3c638e
minouei <mohammad.minooee@gmail.com>
2021-01-14 18:57:28 +0100

    nest

--- configs/publay/faster_rcnn_s50_fpn_syncbn-backbone+head_mstrain-range_cycle_publay.py
+++ configs/publay/faster_rcnn_s50_fpn_syncbn-backbone+head_mstrain-range_cycle_publay.py
@@ -140,7 +140,7 @@ test_cfg = dict(
         nms_thr=0.8,
         min_bbox_size=0),
     rcnn=dict(
-        score_thr=0.1,
+        score_thr=0.3,
         nms=dict(type='nms', iou_threshold=0.5),
         max_per_img=100))
 dataset_type = 'CocoDataset'
