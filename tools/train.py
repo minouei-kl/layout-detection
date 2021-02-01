bd2a142e3009cf8b5f4bb5fef3f5d85fef30cc17
minouei <mohammad.minooee@gmail.com>
2021-02-01 14:57:11 +0100

    print

--- tools/train.py
+++ tools/train.py
@@ -157,7 +157,7 @@ def main():
 
     model = build_detector(
         cfg.model, train_cfg=cfg.train_cfg, test_cfg=cfg.test_cfg)
-    print(model)
+    # print(model)
     datasets = [build_dataset(cfg.data.train)]
     if len(cfg.workflow) == 2:
         val_dataset = copy.deepcopy(cfg.data.val)
