da904f89b129e9d851969e4736ac21876a8438cf
minouei <mohammad.minooee@gmail.com>
2021-02-02 23:31:10 +0100

    pod

---
+++ pod-1x-w.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip uninstall pycocotools -y
+pip install mmpycocotools
+MMCV_WITH_OPS=1 pip install git+file:///netscratch/minouei/report/mmcv
+pip install git+file:///netscratch/minouei/report/mmdetection
+python -u tools/train.py configs/publay/faster_rcnn_s50_fpn_wT_1x_pod.py  --launcher=slurm
